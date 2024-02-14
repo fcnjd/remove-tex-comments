cf06b2f95f696dcf171f1b082311770b97313639
Julian Dreykorn <dreykorn.julian@gmail.com>
2024-02-14 10:39:45 +0000

    Began argparse

---
+++ removecomments.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=str, description='input file to read', required=true)
+    parser.add_argument('-o', '--output', )
